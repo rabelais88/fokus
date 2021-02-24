c9dc72a7ecca3611f02e928099190d5df86e7e7e
rabelais88 <sungryeolp@gmail.com>
2021-02-24 11:32:46 +0900

    :white_check_mark: minor tweak on testing

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,4 +1,6 @@
-it('accessing options.html', () => {
+it('accessing options.html welcoming page', () => {
   cy.visit('/options.html');
   cy.findByRole('logo').should('exist');
+  cy.findByRole('tablist').should('exist');
+  cy.findByRole('tab', { name: 'tasks', selected: true }).should('exist');
 });
