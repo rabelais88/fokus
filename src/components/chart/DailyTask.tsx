4cad9680a6c6e9aecccc98a1f863909d17cbb405
rabelais88 <sungryeolp@gmail.com>
2021-04-28 13:23:21 +0900

    :construction: WIP

--- src/components/chart/DailyTask.tsx
+++ src/components/chart/DailyTask.tsx
@@ -1,12 +1,21 @@
 import makeLogger from '@/lib/makeLogger';
 import { Box } from '@chakra-ui/react';
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+  useMemo,
+} from 'react';
 import * as d3 from 'd3';
+import strToHexColor from '@/lib/_strToHexColor';
 
 interface TaskHistoryChartProps {
   taskHistory: taskHistory[];
+  tasks: tasksData;
   width: number;
   height: number;
+  padding?: number;
 }
 
 interface d3states {
@@ -16,17 +25,23 @@ interface d3states {
 const translate = (x: number = 0, y: number = 0, k: number = 0) => {
   let t = `translate(${x}, ${y})`;
   if (k >= 1) t = [t, `scale(${k})`].join(' ');
-  return k;
+  return t;
 };
+
+const px = (x: number) => `${x}px`;
 
 const logger = makeLogger('DailyTask');
 
 const TaskHistoryChart: React.FC<TaskHistoryChartProps> = (props) => {
-  const { taskHistory, width, height } = props;
+  const { taskHistory, width, height, padding = 0, tasks } = props;
   const refChart = useRef<HTMLDivElement>(null);
   const [nodes, setNodes] = useState<d3states>({
     svg: null,
   });
+  const chartXstart = padding;
+  const chartYstart = padding;
+  const chartXend = useMemo(() => width - padding, [width, padding]);
+  const chartYend = useMemo(() => height - padding, [height, padding]);
 
   useEffect(() => {
     if (refChart.current !== null && refChart.current !== undefined) {
@@ -42,34 +57,69 @@ const TaskHistoryChart: React.FC<TaskHistoryChartProps> = (props) => {
     }
   }, [refChart.current]);
 
-  const enterTask = (scaleY: d3.ScaleLinear<number, number>) => (
-    tsk: d3.Selection<d3.EnterElement, taskHistory, SVGSVGElement, unknown>
-  ) => {
-    return tsk
-      .append('text')
-      .text((d) => JSON.stringify(d))
-      .attr('dominant-baseline', 'hanging')
-      .attr('transform', (d) => translate(0, scaleY(d.timeStart)));
-  };
+  const times = useMemo(
+    () =>
+      taskHistory.reduce(
+        (ac, cv) => [...ac, cv.timeStart, cv.timeEnd],
+        new Array<number>()
+      ),
+    [taskHistory]
+  );
+  const timeMin = useMemo(() => d3.min(times) || -1, [times]);
+  const timeMax = useMemo(() => d3.max(times) || -1, [times]);
+  logger({ timeMin, timeMax });
 
-  function d3render() {
+  const scaleY = useCallback(
+    d3.scaleLinear().domain([timeMin, timeMax]).range([chartYstart, chartYend]),
+    [timeMin, timeMax, height]
+  );
+
+  const drawTaskDot = useCallback(
+    (tsk) =>
+      tsk
+        .append('circle')
+        .attr('r', 14)
+        .attr('cx', 14)
+        .attr('cy', 7)
+        .attr('fill', (d: taskHistory) => strToHexColor(d.taskId)),
+    [tasks]
+  );
+
+  const drawTaskTitle = useCallback(
+    (tsk) =>
+      tsk
+        .append('text')
+        .text((d: taskHistory) => tasks[d.taskId].title)
+        .attr('dominant-baseline', 'hanging'),
+    [tasks]
+  );
+
+  const enterTask = useCallback(
+    (
+      tsk: d3.Selection<d3.EnterElement, taskHistory, SVGSVGElement, unknown>
+    ) => {
+      return tsk
+        .append('g')
+        .attr('class', 'task')
+        .attr('transform', (d: taskHistory) =>
+          translate(chartXstart, scaleY(d.timeStart))
+        )
+        .call(drawTaskDot)
+        .call(drawTaskTitle);
+    },
+    [taskHistory, tasks]
+  );
+
+  const d3render = useCallback(() => {
     if (!nodes.svg) return null;
     logger('render d3');
     nodes.svg.attr('width', width).attr('height', height);
-    const times = taskHistory.reduce(
-      (ac, cv) => [...ac, cv.timeStart, cv.timeEnd],
-      new Array<number>()
-    );
-    const timeMin = d3.min(times) || -1;
-    const timeMax = d3.max(times) || -1;
-
-    const scaleY = d3.scaleLinear<number, number>().domain([timeMin, timeMax]);
     logger(times);
     nodes.svg
-      .selectAll<SVGSVGElement, taskHistory[]>('.text')
+      .selectAll<SVGSVGElement, taskHistory[]>('.task')
       .data(taskHistory)
-      .join(enterTask(scaleY));
-  }
+      .join(enterTask);
+  }, [nodes, taskHistory, tasks]);
 
   d3render();
 
--- src/stories/chart/DailyTaskWrapper.tsx
+++ src/stories/chart/DailyTaskWrapper.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
 import DailyTask from '@/components/chart/DailyTask';
+import { BLOCK_MODE_ALLOW_ALL } from '@/constants';
 
 const DailyTaskWrapper = () => {
   return (
     <DailyTask
       width={500}
       height={500}
+      padding={20}
       taskHistory={[
-        { timeStart: 1000, timeEnd: 3000, taskId: 'ewjkflsjfkl' },
-        { timeStart: 3500, timeEnd: -1, taskId: 'ewjkflsjfkl' },
+        { timeStart: 1000, timeEnd: 3000, taskId: 'task1' },
+        { timeStart: 3500, timeEnd: -1, taskId: 'task2' },
       ]}
+      tasks={{
+        task1: {
+          id: 'task1',
+          emojiId: 'smile',
+          title: 'my first task',
+          description: 'this is my first task',
+          blockedSiteIds: [],
+          allowedSiteIds: [],
+          blockMode: BLOCK_MODE_ALLOW_ALL,
+          maxDuration: 0,
+        },
+        task2: {
+          id: 'task1',
+          emojiId: 'coffee',
+          title: 'my second task',
+          description: 'this is my first task',
+          blockedSiteIds: [],
+          allowedSiteIds: [],
+          blockMode: BLOCK_MODE_ALLOW_ALL,
+          maxDuration: 0,
+        },
+      }}
     />
   );
 };
