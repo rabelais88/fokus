34fb585df83ce64607a138e478a7800d020d8ab7
rabelais88 <sungryeolp@gmail.com>
2021-04-28 12:23:25 +0900

    :building_construction: changed default emoji set as constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,4 @@ export * from './blockMode';
 export * from './storeKey';
 
 export const QUERY_BLOCKED_URL = 'blocked-url';
+export const DEFAULT_EMOJI_SET = 'google'; // 'apple' | 'google' | 'twitter'
