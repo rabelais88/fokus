90d7e806cbdd9c86d14a1fcc6c0de1f5dbaeefad
rabelais88 <sungryeolp@gmail.com>
2021-05-04 18:54:51 +0900

    :white_check_mark: fixed date i18n

--- src/i18n/i18n.test.jsx
+++ src/i18n/i18n.test.jsx
@@ -3,14 +3,15 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import i18n from '@/i18n';
+import analyzeTime from '@/lib/analyzeTime';
 
 const TimeFormatApp = () => {
   const { t } = useTranslation();
-  const time = new Date('2020-01-01 00:00').getTime();
+  const time = analyzeTime(new Date('2020-01-01 00:00').getTime());
   return (
     <div>
       <h1>{t('tab-tasks')}</h1>
-      <p data-testid="date">{t('daily-task-time-format', { time })}</p>
+      <p data-testid="date">{t('hour-minute', time)}</p>
     </div>
   );
 };
