e386cce21c8d45783eadecb35a3aaccc9a177a2b
Park Sungryeol <sungryeolp@gmail.com>
2021-06-14 16:11:05 +0900

    Automatic build and release from Drone.io (#35)
    * :construction: WIP - stuck with IDB issues
    
    * :construction: WIP
    
    * :construction: WIP
    
    * :construction: WIP on SWR
    
    * :construction: WIP
    
    * :construction: WIP
    
    * :construction: WIP
    
    * WIP
    
    * :construction: WIP
    
    * :bug: fixed bug related to in-line key using
    
    * :construction: WIP
    
    * :bug: loop related bug fix
    
    * :construction: modified console log
    
    * :bookmark: versioning
    
    * :bulb: changed comment
    
    * :bug: fixed debug mode checkbox bug
    
    * :construction: WIP
    
    * :bug: fixed bug task not closing properly
    
    * :bug: fixed IDBKeyRange bound issues
    
    * :bug: fixing file export - import issues
    
    * :bug: fixing bug
    
    * :construction: WIP
    
    * :construction: WIP
    
    * :construction: WIP
    
    * :construction: WIP
    
    gaa
    
    * added revalidation for import-export
    
    * added reusable modal context
    
    * rewriting test WIP
    
    * improved build performance
    
    * addded tutorial & build performance improved
    
    * fixed minor bugs in tutorial
    
    * WIP
    
    * updated tutorial
    
    * :white_check_mark: fixed block mode unit test
    
    * :white_check_mark: updated ci-cd test
    
    * :construction_worker: added automatic build system for CI-CD
    
    * :green_heart: fixing ci error
    
    * :green_heart: fixed console messages for CI
    
    * :green_heart: streamlining CI for better performance
    
    * :green_heart: fixing cypress ci issues
    
    * :green_heart: fixing cypress related ci issue
    
    * :green_heart: fixed wrong cypress install
    
    * :green_heart: fixing ci

---
+++ check-version.sh
@@ -0,0 +1,36 @@
+LATEST_VERSION=$(git ls-remote --exit-code --refs --tags https://github.com/rabelais88/fokus.git | tail -1 | awk '{print $2}' | grep -Eo "[\.0-9]+")
+echo "latest version on github: ${LATEST_VERSION}"
+NOW_VERSION=$(cat ./src/manifest.json | grep -Eo '"version": "([\.0-9]+)"' | grep -Eo "[\.0-9]+")
+echo "current version on manifest.json: ${NOW_VERSION}"
+if [ "$DRONE_COMMIT_BRANCH" != "develop" ]; then
+	echo "build is not allowed in non-develop branch: ${DRONE_COMMIT_BRANCH}"
+	exit 0
+fi
+if [ "$LATEST_VERSION" != "$NOW_VERSION" ]; then
+	echo "build started"
+	apt-get update
+	apt install zip
+	git clone https://github.com/rabelais88/fokus fokus-build
+	cd fokus-build
+	# hide secrets from console
+	# must use escape secret value with $$
+	GIT_URL=https://rabelais88:$${GITHUB_TOKEN}@github.com/rabelais88/fokus.git printenv GIT_URL | git remote set-url origin
+	git checkout master
+	git pull origin develop
+	# delete pre-existing release file
+	rm -rf release
+	rm -f *.zip
+	yarn install
+	yarn build
+	$VERSION_TAG="v${NOW_VERSION}"
+	mv build release
+	zip -r ${VERSION_TAG}.zip release
+	git add .
+	git commit -am "versioning ${VERSION_TAG} for release"
+	git push origin master
+	git tag -a $VERSION_TAG -m "build version ${VERSION_TAG} from drone.io"
+	git push origin $VERSION_TAG
+	echo "build success!"
+else
+	echo "nothing to build."
+fi
